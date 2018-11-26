43d743ef65e5aece4fdfd6a313564de7fea2f83a
G4nsch <45365675+G4nsch@users.noreply.github.com>
2018-11-26 18:54:12 +0100

    Create servas.c
    The servas file.

---
+++ servas.c
@@ -0,0 +1,13 @@
+//////////////////////////*
+author:        Gansch
+create date:   26.11.2018
+last edit:     26.11.2018
+title:         notitle
+*//////////////////////////
+#include <std/io.h>
+
+int main(void)
+{
+printf(" Servas!");
+return 0;
+}
