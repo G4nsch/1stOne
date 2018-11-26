3602ef1ba378eef3d2a038b694a510cfb6b1a9bf
G4nsch <45365675+G4nsch@users.noreply.github.com>
2018-11-26 19:38:19 +0100

    Add files via upload

---
+++ hallo.cpp
@@ -0,0 +1,17 @@
+/**************************
+author:        Gansch
+create date:   26.11.2018
+last edit:     26.11.2018
+title:         C++
+**************************/
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hallo!" << endl;
+	return 0;
+}
