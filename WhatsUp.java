d722d9e02f279bdffc359b05706e094f6e1f23a6
G4nsch <45365675+G4nsch@users.noreply.github.com>
2018-11-26 19:45:21 +0100

    Add files via upload
    added WhatsUp.java

---
+++ WhatsUp.java
@@ -0,0 +1,17 @@
+/**************************
+author:        Gansch
+create date:   26.11.2018
+last edit:     26.11.2018
+title:         java
+**************************/
+
+public class WhatsUp{
+
+	public static void main(String [] args){
+
+		System.out.println(" Whats up?");
+
+
+	}
+
+}
